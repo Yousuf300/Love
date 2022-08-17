0036a824a3708f174f1036abc02751b3d4671db8
unknown <freelanceryousuf89@gmail.com>
2022-08-17 12:47:05 +0600

    first commit

---
+++ love.js
@@ -0,0 +1,12 @@
+document.getElementById('yes').addEventListener("click", function () {
+    common('msg')
+    document.getElementById('msg').innerText = " I Love U 🏩  "
+})
+function loveletter() {
+    common('msg')
+    //  document.getElementById('msg').innerText = ' I well merry to You 🗯  '
+}
+// daynamice
+function common(id) {
+    document.getElementById(id).innerText = "I Love U Sakib miya 🌹 "
+}
